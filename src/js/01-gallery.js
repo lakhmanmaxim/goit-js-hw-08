187aa9f656a1bafca2c1e94c003c870de169010b
Maxim Lakhman <lakhmanmaxim@gmail.com>
2022-11-18 22:48:15 +0200

    1-е задание готово
    1-е задание готово

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.js';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// =============================  Создание разметки - НАЧАЛО
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source= ${original}
+      alt=${description}
+      title=${description}
+    />
+  </a>
+    `;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// =============================== Создание разметки - КОНЕЦ
+
+//================ Модальное окно
+
+const modal = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+// ============ Отмена действия по умолчанию - для ссылки
+
+const refEl = document.querySelector('.gallery__item');
+refEl.addEventListener('click', refElPreventDefault);
+
+function refElPreventDefault(e) {
+  e.preventDefault();
+}
