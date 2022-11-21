60eec849d70967ad83f1be605b60a7ee8e2c031e
Maxim Lakhman <lakhmanmaxim@gmail.com>
2022-11-21 19:49:09 +0200

    Задание 2 сделано
    Задание 2 сделано

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const VIDEO_CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(setVideoCurrentTimeToStorage, 1000));
+
+function setVideoCurrentTimeToStorage() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(VIDEO_CURRENT_TIME_KEY, seconds);
+  });
+}
+
+player.setCurrentTime(localStorage.getItem(VIDEO_CURRENT_TIME_KEY));
